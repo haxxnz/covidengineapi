0e63d1705affa6100aa1c3d58ff1c112b5e0ae85
minh <Lminhphu0304@gmail.com>
2021-08-21 09:55:04 +1200

    :eye: :lips: :eye:

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -2,6 +2,7 @@ import { Kafka, SASLOptions } from 'kafkajs';
 
 import { runConsumer } from './connection';
 
+// THRISSSSSS!!!!!!! DON'T COMMIT CREDENTIAL TO REPO
 const kafkaConfig = { username: '7M2SM5KA554SODLD', password: '3y9gqYEpewpwHrMOMBK/xl1R1+yWqUNtc6nOaWmAOZOXPGA0V74eaDPL7N6j32jm' };
 const sasl: SASLOptions | undefined = (kafkaConfig.username && kafkaConfig.password) ? { username: kafkaConfig.username, password: kafkaConfig.password, mechanism: 'plain' } : undefined;
 const ssl = !!sasl;
@@ -9,7 +10,7 @@ const ssl = !!sasl;
 // This creates a client instance that is configured to connect to the Kafka broker provided by
 // the environment variable KAFKA_BOOTSTRAP_SERVER
 const kafka = new Kafka({
-  clientId: '1449-govhack-team',
+  clientId: 'govhack-team-1449',
   brokers: ['pkc-4vndj.australia-southeast1.gcp.confluent.cloud:9092'],
   ssl,
   retry: {
@@ -20,7 +21,7 @@ const kafka = new Kafka({
 
 // Lol 
 const covidConsumer = kafka.consumer({
-  groupId: 'govhack-covid_cases_postcode',
+  groupId: 'govhack-team-1449',
 });
 
 runConsumer(covidConsumer);
