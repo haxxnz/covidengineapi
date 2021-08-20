3de78a42138db3aa6424df3ac7259ee5943f9932
minh <Lminhphu0304@gmail.com>
2021-08-21 10:41:57 +1200

    :eye: :lips: :eye:

--- src/consumer/connection.ts
+++ src/consumer/connection.ts
@@ -1,12 +1,11 @@
 import { Consumer } from 'kafkajs';
 
-import { handleSingeMessage, handleBatchMessage } from './handle';
+import { handleSingeMessage } from './handle';
 
 export async function runConsumer(covidConsumer: Consumer): Promise<void> {
   await covidConsumer.connect();
   await covidConsumer.subscribe({ topic: 'govhack-covid_cases_postcode', fromBeginning: true });
   await covidConsumer.run({
     eachMessage: handleSingeMessage,
-    eachBatch: handleBatchMessage,
   });
 }
--- src/consumer/handle.ts
+++ src/consumer/handle.ts
@@ -1,14 +1,21 @@
 import { EachBatchPayload, EachMessagePayload, Message } from 'kafkajs';
 
+type CovidMessage = {
+  objectid: string
+  data_date: string
+  postcode: string
+  new: number | null
+  active: number | null
+  rate: number | null
+}
+
 export async function handleSingeMessage({ topic, partition, message }: EachMessagePayload) {
+  const messageValue: CovidMessage = JSON.parse(message?.value?.toString() || "")
   console.log({
     key: message.key.toString(),
     value: message?.value?.toString(),
     headers: message.headers,
+
   });
   throw new Error("Shit man"); // Throw error so that it doesn't commit offset
-}
-
-export async function handleBatchMessage(payload: EachBatchPayload) {
-  throw new Error("Don not fucking commit message");  // Throw error so that it doesn't commit offset
 }
