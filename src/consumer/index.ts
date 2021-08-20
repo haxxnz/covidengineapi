5fb0ce459808735bfffb93037cff1599af60c312
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-08-21 08:23:42 +1000

    .env

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -1,11 +1,21 @@
-import { Kafka, SASLOptions } from 'kafkajs';
+import { Kafka, SASLOptions } from 'kafkajs'
+import { runConsumer } from './connection'
 
-import { runConsumer } from './connection';
+const { KAFKA_USERNAME, KAFKA_PASSWORD } = process.env
 
-// THRISSSSSS!!!!!!! DON'T COMMIT CREDENTIAL TO REPO
-const kafkaConfig = { username: '7M2SM5KA554SODLD', password: '3y9gqYEpewpwHrMOMBK/xl1R1+yWqUNtc6nOaWmAOZOXPGA0V74eaDPL7N6j32jm' };
-const sasl: SASLOptions | undefined = (kafkaConfig.username && kafkaConfig.password) ? { username: kafkaConfig.username, password: kafkaConfig.password, mechanism: 'plain' } : undefined;
-const ssl = !!sasl;
+const kafkaConfig = {
+  username: KAFKA_USERNAME,
+  password: KAFKA_PASSWORD,
+}
+const sasl: SASLOptions | undefined =
+  kafkaConfig.username && kafkaConfig.password
+    ? {
+        username: kafkaConfig.username,
+        password: kafkaConfig.password,
+        mechanism: 'plain',
+      }
+    : undefined
+const ssl = !!sasl
 
 // This creates a client instance that is configured to connect to the Kafka broker provided by
 // the environment variable KAFKA_BOOTSTRAP_SERVER
@@ -14,19 +24,16 @@ const kafka = new Kafka({
   brokers: ['pkc-4vndj.australia-southeast1.gcp.confluent.cloud:9092'],
   ssl,
   retry: {
-    retries: 100
+    retries: 100,
   },
-  sasl
-});
+  sasl,
+})
 
-// Lol 
+// Lol
 const covidConsumer = kafka.consumer({
   groupId: 'govhack-team-1449',
-});
+})
 
-runConsumer(covidConsumer);
+runConsumer(covidConsumer)
 
-export {
-  covidConsumer,
-  kafka
-};
+export { covidConsumer, kafka }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+require('dotenv').config()
 
 const app = express()
 const port = 3001
